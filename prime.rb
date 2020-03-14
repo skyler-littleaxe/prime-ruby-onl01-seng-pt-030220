6c87e8e0960131d4f0cece3ff8dc646a0727eb8c
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-14 21:46:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+#def prime?(number)
+ #if number < 1 
+  #  false
+   # elsif number == (1||2||3)
+  #  true
+  #elseif number % 
+    
+  #end
+#end
+
+def prime?(number)
+    i = 2
+   return false if number <= 1
+    while i < number
+        return false if number % i == 0 || number < 1
+        i += 1
+    end
+    true
+end
