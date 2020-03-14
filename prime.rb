52349bd8ff5d67fbbad498309cac51ccefe21a64
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-14 21:44:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -12,7 +12,7 @@
 def prime?(number)
     i = 2
    return false if number <= 1
-    while n < integer
+    while i < integer
         return false if integer % i == 0 || integer < 1
         i += 1
     end
